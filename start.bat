364b7a16d9ef12258df0db7b504868595a6b15bc
Pupcheg <santos43241@gmail.com>
2024-06-10 04:39:58 +0300

    Updated start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,1 @@
-cd C:\Users\Supcheg\IdeaProjects\seabattle
-"C:\Program Files\Java\jdk-21\bin\java.exe" -jar "C:\Users\Supcheg\IdeaProjects\seabattle\build\libs\seabattle-1.0-SNAPSHOT.jar"
+java -jar "build\libs\seabattle-1.0-SNAPSHOT.jar"
